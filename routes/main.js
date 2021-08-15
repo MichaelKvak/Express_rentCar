9b8f6d278b75caee79d6fc09aba3d5953471d20c
MichaelKvak <mkvak1987@gmail.com>
2021-08-15 22:14:01 +0300

    the end

--- routes/main.js
+++ routes/main.js
@@ -23,6 +23,32 @@ router.get("/", function (req, res, next) {
       "automatic transmission",
       "electro engine",
     ],
+    arrayAboutCar: [
+      "Engine volume",
+      "Power, h.p. / rev.min",
+      "Torque, Nm / rpm",
+      "Compression",
+      "Fuel tank volume, liters",
+      "Maximum speed, km / h",
+      "Acceleration from 0 to 100/200 km/h, sec",
+      "City consumption, l/100 km",
+      "Highway consumption, l/100 km",
+      "Combined cycle, l/100 km",
+      "CO2 emissions g/km",
+    ],
+    arrayAboutCar2: [
+      "4395",
+      " 600/5600-6700",
+      " 750/1800-5600",
+      "10.0",
+      "68",
+      " 250/305",
+      "3.4/11.1",
+      "14.5",
+      "8.2",
+      "10.5",
+      "241",
+    ],
   });
 });
 
